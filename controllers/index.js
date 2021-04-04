41c4bd71665de47c2cba2cfba16df9988be0950c
Matthew A. John <mattj900@outlook.com>
2021-04-03 20:58:58 -0700

    Update

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
-const api = require("./api")
-const homeRoutes =require("./homeRoutes")
-router.use('/api', api);
-router.use("/",homeRoutes);
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
 module.exports = router;
+
+
+
+// const router = require('express').Router();
+// const api = require("./api")
+// const homeRoutes =require("./homeRoutes")
+// router.use('/api', api);
+// router.use("/",homeRoutes);
+// module.exports = router;
